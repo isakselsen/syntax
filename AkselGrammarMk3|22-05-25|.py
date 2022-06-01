9803e5a5a1bdcdaac76a8e128e069f074bc1427d
Isaiah Akselsen <106271587+isakselsen@users.noreply.github.com>
2022-06-01 14:16:47 -0700

    Update AkselGrammarMk3|22-05-25|.py
    Expanded the phrase possibilities.  It can now generate proper deep structures, though not all at once.  Working on a way to generate a full sentence all at once; That's "GenPhraseToNodeMP" on line 142.  It doesn't work yet.

--- AkselGrammarMk3|22-05-25|.py
+++ AkselGrammarMk3|22-05-25|.py
@@ -67,26 +67,27 @@ DEG  = "degree"
 NAME = "name"
 PRON = "pronoun"
 POSS = "possessive marker"
+NULL = ""
 # Phrase Structures
 NEGP  = ['NEGbar']
-MEASP = []
-DEGP  = ['DEGbar', 'AP']
-PP    = ['MEASP', 'Pbar']
+MEASP = ['UNKNOWN']
+DEGP  = [['DEGbar', 'AP']]
+PP    = [['MEASP', 'Pbar']]
 DP    = [['NP', 'POSS'], D]
-NP    = ['PRON', 'NAME', 'Nbar', ['DP', 'Nbar'], ['DEGP']]
-MP    = [['NP', 'Mbar'], ['CP', 'Nbar']]
+NP    = ['PRON', 'NAME', 'Nbar', ['DP', 'Nbar'], ['DEGP', 'Nbar'], ['DP', 'DEGP', 'Nbar']]
+MP    = [['NP', 'Mbar'], ['Mbar']]
 VP    = [[V, 'VP'], ['Vbar']]
-AP    = ['Abar']
-CP    = ['Cbar']
+AP    = [['Abar']]
+CP    = [['Cbar']]
 # Prime Structures
-NEGbar = ['NEG', 'VP']
-DEGbar = ['DEG', 'AP']
+NEGbar = [['NEG', 'VP']]
+DEGbar = [['DEG', 'AP']]
 Mbar   = [[M, 'VP'], [M, 'NEGP', 'VP']]
-Nbar   = [N, 'PP', 'CP']
+Nbar   = [[N, 'PP', 'CP']]
 Pbar   = [[P, 'NP'], [P, 'MP'], [P, 'PP']]
-Vbar   = [V]
-Abar   = [A, 'PP', 'CP']
-Cbar   = [C, 'MP']
+Vbar   = [[V], [V, 'NP'], [V, 'NP', 'PP'], [V, 'PP']]
+Abar   = [[A, 'PP', 'CP']]
+Cbar   = [[C, 'MP']]
 
 
 
@@ -100,39 +101,168 @@ def GetVerb():                                           #returns verb
 def GetNoun():                                           #returns noun
     return random.choice(list(NounDict.keys()))
 
+def GenerateNEGP():
+    return random.choice(NEGP)
+def GenerateMEASP():
+    return random.choice(MEASP)
+def GenerateDEGP():
+    return random.choice(DEGP)
+def GeneratePP():
+    return random.choice(PP)
+def GenerateDP():
+    return random.choice(DP)
 def GenerateMP():
     return random.choice(MP)  #this is generating the empty nodes instead of the markers that eventually represent those nodes
+def GenerateNP():
+    return random.choice(NP) 
+def GenerateVP():
+    return random.choice(VP)
+def GenerateAP():
+    return random.choice(AP)
+def GenerateCP():
+    return random.choice(CP)
+
+def GenerateNEGbar():
+    return random.choice(NEGbar)
+def GenerateDEGbar():
+    return random.choice(DEGbar)
+def GeneratePbar():
+    return random.choice(Pbar)
+def GenerateMbar():
+    return random.choice(Mbar)
+def GenerateNbar():
+    return random.choice(Nbar)
+def GenerateVbar():
+    return random.choice(Vbar)
+def GenerateAbar():
+    return random.choice(Abar)
+def GenerateCbar():
+    return random.choice(Cbar)
+
+def GenPhraseToNodeMP():
+    EMPEE = GenerateMP()
+    for ConstitL1 in EMPEE:
+        if ConstitL1 == ['NP', 'Mbar']:
+            for ConstitL2 in ConstitL2:
+                if ConstitL2 == 'NP':
+                    ConstitL2 = GenerateNP()
+                elif ConstitL2 == 'Mbar':
+                    ConstitL2 = GenerateMbar()
+        elif ConstitL1 == ['Mbar']:
+            ConstitL1 = GenerateMbar()
+    return EMPEE
 
 
 
-print('Would you like to run a full test gamut? {y/n}')
-
-if input() == 'y':
-    print('The phrases:')
-    print(NEGP, MEASP, DEGP, PP, DP)
-    print(NP, MP, VP, AP, CP)
-    print('The bars:')
-    print(NEGbar, DEGbar, Mbar, Nbar, Pbar, Vbar, Abar, Cbar)
-    print('Checking the \'get\' statements:')
-    print(GetDeterminer, GetModal, GetVerb, GetNoun)
-else:
-    print('Would you like to test the phrases?')
-    if input() == 'y':
+def Query():
+    print('What would you like to do?')
+    request = input()
+    if request == 'full test':
         print('The phrases:')
-        print(NEGP, MEASP, DEGP, PP, DP)
-    else:
-        print('Would you like to test the bars?')
-        if input() == 'y':
-            print('The bars:')
-            print(NEGbar, DEGbar, Mbar, Nbar, Pbar, Vbar, Abar, Cbar)
-        else:
-            print('Would you like to test the get statements?')
-            if input() == 'y':
-                    print(GetDeterminer, GetModal, GetVerb, GetNoun)
+        print(f'NEGP = {NEGP}, MEASP = {MEASP}, DEGP = {DEGP}, PP = {PP}, DP = {DP}, \nNP = {NP}, MP = {MP}, \nVP = {VP}, AP = {AP}, CP = {CP}')
+        print('The bars:')
+        print(f'NEGbar = {NEGbar}, DEGbar = {DEGbar}, Mbar = {Mbar}, Nbar = {Nbar}, \nPbar = {Pbar}, Vbar = {Vbar}, Abar = {Abar}, \nCbar = {Cbar}')
+        print('Checking the \'get\' statements:')
+        print(GetDeterminer(), GetNoun(), GetModal(), GetVerb())
+
+    elif request == 'phrase test':
+        print('The phrases:')
+        print(f'NEGP = {NEGP}, MEASP = {MEASP}, DEGP = {DEGP}, PP = {PP}, DP = {DP}, \nNP = {NP}, MP = {MP}, \nVP = {VP}, AP = {AP}, CP = {CP}')
+    elif request == 'bar test':
+        print('The bars:')
+        print(f'NEGbar = {NEGbar}, DEGbar = {DEGbar}, Mbar = {Mbar}, Nbar = {Nbar}, \nPbar = {Pbar}, Vbar = {Vbar}, Abar = {Abar}, \nCbar = {Cbar}')
+    elif request == 'get test':
+        print(GetDeterminer(), GetNoun(), GetModal(), GetVerb())
+    
+    elif request == 'MP':
+        print('How many MPs would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateMP())
+            numPs -= 1
+    elif request == 'NP':
+        print('How many NPs would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateNP())
+            numPs -= 1
+    elif request == 'VP':
+        print('How many NPs would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateVP())
+            numPs -= 1
+    elif request == 'CP':
+        print('How many CPs would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateCP())
+            numPs -= 1
+    elif request == 'PP':
+        print('How many PPs would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GeneratePP())
+            numPs -= 1
+
+    elif request == 'Pbar':
+        print('How many Pbars would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GeneratePbar())
+            numPs -= 1
+    elif request == 'Nbar':
+        print('How many Nbars would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateNbar())
+            numPs -= 1
+    elif request == 'Cbar':
+        print('How many Cbars would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateCbar())
+            numPs -= 1
+    elif request == 'Mbar':
+        print('How many Mbars would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateMbar())
+            numPs -= 1
+    elif request == 'Vbar':
+        print('How many Vbars would you like to print?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenerateVbar())
+            numPs -= 1
+
+    elif request == 'P2N MP':
+        print('How many MPs would you like to generate to the nodes?')
+        numPs = int(input())
+        while numPs !=0:
+            print(GenPhraseToNodeMP())
+            numPs -= 1
+
+    print('\nAnything else? (y/n)')
+    if input() == 'y':
+        Query()
+
+    
 
 
-                    
-                    
+print('')
+print('')
+
+Query()
+
+print('\nThank you.  Program ended.')
+
+print('')
+print('')
+
+
+
+
 
 
 
